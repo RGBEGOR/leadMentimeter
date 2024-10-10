4ffad503c5148c63b0fa9dd3589a4ab11c6adb1c
RGBEGOR <141853566+RGBEGOR@users.noreply.github.com>
2024-10-10 21:56:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,266 @@
+const headElem = document.getElementById("head");
+const buttonsElem = document.getElementById("buttons");
+const pagesElem = document.getElementById("pages");
+
+class Quiz
+{
+	constructor(type, questions, results)
+	{
+
+		this.type = type;
+
+		this.questions = questions;
+
+		this.results = results;
+
+		this.score = 0;
+
+		this.result = 0;
+
+		this.current = 0;
+	}
+
+	Click(index)
+	{
+		let value = this.questions[this.current].Click(index);
+		this.score += value;
+
+		let correct = -1;
+
+
+		if(value >= 1)
+		{
+			correct = index;
+		}
+		else
+		{
+
+			for(let i = 0; i < this.questions[this.current].answers.length; i++)
+			{
+				if(this.questions[this.current].answers[i].value >= 1)
+				{
+					correct = i;
+					break;
+				}
+			}
+		}
+
+		this.Next();
+
+		return correct;
+	}
+
+
+	Next()
+	{
+		this.current++;
+		
+		if(this.current >= this.questions.length) 
+		{
+			this.End();
+		}
+	}
+
+
+	End()
+	{
+		for(let i = 0; i < this.results.length; i++)
+		{
+			if(this.results[i].Check(this.score))
+			{
+				this.result = i;
+			}
+		}
+	}
+} 
+
+
+class Question 
+{
+	constructor(text, answers)
+	{
+		this.text = text; 
+		this.answers = answers; 
+	}
+
+	Click(index) 
+	{
+		return this.answers[index].value; 
+	}
+}
+
+
+class Answer 
+{
+	constructor(text, value) 
+	{
+		this.text = text; 
+		this.value = value; 
+	}
+}
+
+
+class Result 
+{
+	constructor(text, value)
+	{
+		this.text = text;
+		this.value = value;
+	}
+
+
+	Check(value)
+	{
+		if(this.value <= value)
+		{
+			return true;
+		}
+		else 
+		{
+			return false;
+		}
+	}
+}
+
+
+const results = 
+[
+	new Result("Ну это прям позор", 0),
+	new Result("Ну уже неплохо", 3),
+	new Result("Ваш уровень выше среднего", 4),
+	new Result("Поздравляем Вы успешно усвоили эту тему", 5)
+];
+
+const questions = 
+[
+	new Question(" Первый независимый от халифата эмират образовался в 756 году в", 
+	[
+		new Answer('Франция ', 0),
+		new Answer("Испания ", 1),
+		new Answer("Сирия", 0),
+		new Answer("Иран", 0)
+	]),
+	new Question("Какая династия правила раньше Аббасиды или Омеяды", 
+	[
+		new Answer('Аббасиды', 1),
+		new Answer("Омеяды", 0),
+		new Answer("Османы", 0),
+		new Answer("Не знаю", 0)
+	]),
+	new Question("Как стали называться правители Халифата после смерти Мухаммеда ", 
+	[
+		new Answer('не знаю', 0),
+		new Answer("Кадии", 0),
+		new Answer("Имамы", 0),
+		new Answer("Халифы", 1)
+	]),
+	new Question("В каком году Мухаммед перебрался из Мекки в Медину?", 
+	[
+		new Answer('522 ', 0),
+		new Answer("622", 1),
+		new Answer("822", 0),
+		new Answer("999", 0)
+	]),
+	new Question("Как называется правовая система ислама?", 
+	[
+		new Answer('Шариат ', 1),
+		new Answer("Ислам", 0),
+		new Answer("Канон", 0),
+		new Answer("нет ответа", 0)
+
+	]),
+];
+
+
+const quiz = new Quiz(1, questions, results);
+
+Update();
+
+
+function Update()
+{
+
+	if(quiz.current < quiz.questions.length) 
+	{
+
+		headElem.innerHTML = quiz.questions[quiz.current].text;
+
+
+		buttonsElem.innerHTML = "";
+
+
+		for(let i = 0; i < quiz.questions[quiz.current].answers.length; i++)
+		{
+			let btn = document.createElement("button");
+			btn.className = "button";
+
+			btn.innerHTML = quiz.questions[quiz.current].answers[i].text;
+
+			btn.setAttribute("index", i);
+
+			buttonsElem.appendChild(btn);
+		}
+		
+
+		pagesElem.innerHTML = (quiz.current + 1) + " / " + quiz.questions.length;
+
+		Init();
+	}
+	else
+	{
+
+		buttonsElem.innerHTML = "";
+		headElem.innerHTML = quiz.results[quiz.result].text;
+		pagesElem.innerHTML = "Ваши очки: " + quiz.score;
+	}
+}
+
+function Init()
+{
+
+	let btns = document.getElementsByClassName("button");
+
+	for(let i = 0; i < btns.length; i++)
+	{
+
+		btns[i].addEventListener("click", function (e) { Click(e.target.getAttribute("index")); });
+	}
+}
+
+function Click(index) 
+{
+
+	let correct = quiz.Click(index);
+
+
+	let btns = document.getElementsByClassName("button");
+
+
+	for(let i = 0; i < btns.length; i++)
+	{
+		btns[i].className = "button button_passive";
+	}
+
+	if(quiz.type == 1)
+	{
+		if(correct >= 0)
+		{
+			btns[correct].className = "button button_correct";
+		}
+
+		if(index != correct) 
+		{
+			btns[index].className = "button button_wrong";
+		} 
+	}
+	else
+	{
+
+		btns[index].className = "button button_correct";
+	}
+
+
+	setTimeout(Update, 1000);
+}
+
+
